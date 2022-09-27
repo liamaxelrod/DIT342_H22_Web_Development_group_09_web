90ab03c06ffdf138bb4f668c91c334b5daee9d5c
Liam Axelrod <liamaxelrod@gmail.com>
2022-09-27 08:09:45 +0200

    basic table for scheduling
    just the basic HTML scheduling not complete
    mainly because I think there's maybe a better way of making it

--- client/src/router.js
+++ client/src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import Scheduling from './views/Scheduling.vue'
 
 Vue.use(Router)
 
@@ -12,6 +13,11 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/scheduling',
+      name: 'scheduling',
+      component: Scheduling
     }
   ]
 })
