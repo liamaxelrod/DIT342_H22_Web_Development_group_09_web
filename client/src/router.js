3392c4809abc919269598f12de7a9b70570cee79
Yajiri <90855500+Yajiri@users.noreply.github.com>
2022-09-29 16:49:54 +0200

    Front end log in page and connection to forgott password, register and profile, will do the forgot password and register front end during the weekend

--- client/src/router.js
+++ client/src/router.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import Register from './views/Register.vue'
+import ForgotPassword from './views/ForgotPassword.vue'
 
 Vue.use(Router)
 
@@ -12,6 +14,16 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: Register
+    },
+    {
+      path: '/forgotpassword',
+      name: 'forgotPassword',
+      component: ForgotPassword
     }
   ]
 })
