9708c1e6989a3e8e43062474cbac765c695c93f2
jason <gusaxelrli@student.gu.se>
2022-10-07 12:45:35 +0200

    m

--- server/models/schedule.js
+++ server/models/schedule.js
@@ -3,7 +3,7 @@ var Schema = mongoose.Schema;
 
 //will be altered depending on the front is structured
 var scheduleSchema = new Schema({
-    cells: Object
+    monday: Object
 });
 
 module.exports = mongoose.model('schedules', scheduleSchema);
