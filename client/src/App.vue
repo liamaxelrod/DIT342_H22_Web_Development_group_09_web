e90de69e6ac5e1a4fdae820a37525f4629107b7b
Yajiri <90855500+Yajiri@users.noreply.github.com>
2022-10-11 10:50:54 +0200

    updates to sign in

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div id="nav">
       <router-link to="/"></router-link>
-      <router-link to="/schedulingSubmit"></router-link>
+      <router-link to="/schedulingSubmit">schedulingSubmit</router-link>
       <router-link to="/scheduling"></router-link>
     </div>
     <!-- Render the content of the current page view -->
