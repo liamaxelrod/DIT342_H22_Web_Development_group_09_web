54534601d34aed26caa11126442bfa9e0c9c36f0
Aditya-Nair-gu <aadi.nair@gmail.com>
2022-10-13 13:09:40 +0200

    user profile, logout, some other changes'

--- server/models/schedule.js
+++ server/models/schedule.js
@@ -3,16 +3,16 @@ var Schema = mongoose.Schema;
 
 //will be altered depending on the front is structured
 var scheduleSchema = new Schema({
-    owner: '',
-    scheduleName: '',
-    cells: Object
-    // monday: Object,
-    // tuesday: Object,
-    // wednesday: Object,
-    // thursday: Object,
-    // friday: Object,
-    // saturday: Object,
-    // sunday: Object
+  owner: "",
+  scheduleName: { type: String, required: true, unique: true },
+  cells: Object,
+  // monday: Object,
+  // tuesday: Object,
+  // wednesday: Object,
+  // thursday: Object,
+  // friday: Object,
+  // saturday: Object,
+  // sunday: Object
 });
 
 module.exports = mongoose.model("schedules", scheduleSchema);
